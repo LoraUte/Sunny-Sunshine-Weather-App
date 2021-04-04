c6eedf2b6bf667dad458958834e0b8d56a15b549
LoraUte <66828824+LoraUte@users.noreply.github.com>
2021-04-05 00:03:54 +0200

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -22,37 +22,56 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+return days[day];
+}
+
+function displayForecast(response) {
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
-    let days = ["Sun", "Mon", "Tue", "Wed"];
+    
     let forecastHTML = `<div class="row">`;
-    days.forEach(function (day) {
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
     forecastHTML = forecastHTML + 
     `
     <div class="col-2">
         <div class="weather-forecast-date">
-            ${day}
+            ${formatDay(forecastDay.dt)}
         </div>
             <img 
-                src="http://openweathermap.org/img/wn/50d@2x.png" 
+                src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
                 alt="" 
                 width="42"
             />
     <div class="weather-forecast-temperature">
         <span class="weather-forecast-temperature-max">
-            18
+            ${Math.round(forecastDay.temp.max)}°
         </span> 
             <span class="weather-forecast-temperature-min">
-                12
+                ${Math.round(forecastDay.temp.min)}°
             </span>
         </div>
     </div>
     `;
+    }
     });
     
-
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "6e32c0b8c11d15abeec4c1e1d86ae403";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -77,7 +96,9 @@ function displayTemperature(response) {
         `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute(
         "alt", 
-        response.data.weather[0].description);
+        response.data.weather[0].description);   
+
+        getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -121,4 +142,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Ney York");
-displayForecast();
